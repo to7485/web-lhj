ed4d6962471f33248e60a6c1f0d1e894d75e9cc7
to7485 <54658614+to7485@users.noreply.github.com>
2026-04-07 12:20:40 +0900

    0407

--- java/ex_0403/src/ex1_exception/throwsexample/ThrowsExample.java
+++ java/ex_0403/src/ex1_exception/throwsexample/ThrowsExample.java
@@ -15,6 +15,50 @@ public class ThrowsExample {
 		//java.lang.String2클래스를 찾아서 메모리에 올리는 기능
 		Class.forName("java.lang.String2");
 	}
+	
+	//인출하는 기능
+	//흐름이 끊기지는 않음
+	//호출한쪽에서 결과를 직접판단해야 한다.
+//	public static boolean widthdraw(int money) {
+//		if(money <= 0) {
+//			System.out.println("금액 오류");
+//			return false;
+//		}
+//		
+//		if (balance < money) {
+//			System.out.println("잔액 부족");
+//			return false;
+//		}
+//		
+//		balance -= money;
+//		return true;
+//	}
+	
+	//예외처리 방식
+	//잘못된 상황이면 강제로 중단
+	//호출한쪽에서 try-catch로 처리
+	public static void widthdraw(int money) throws Exception {
+		if(money <= 0) {
+			throw new IllegalArgumentException("금액 오류");
+		}
+		
+		if (balance < money) {
+			throw new IllegalArgumentException("잔액 부족");
+		}
+		
+		balance -= money;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
 
 
